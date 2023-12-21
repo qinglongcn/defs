6d14ce1a0b76918a6ed8a19c5cb630fb08ac794e
qinglongcn <1435293080@qq.com>
2023-12-21 11:34:44 +0800

    1221

--- fsnotifys/watcher.go
+++ fsnotifys/watcher.go
@@ -3,39 +3,28 @@
 package fsnotify
 
 import (
-	"bpfs/backend/config"
-	"bpfs/pkg/dir"
-	"context"
 	"fmt"
-	"path/filepath"
 
-	"github.com/fsnotify/fsnotify"
-	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/bpfs/defs/paths"
 	"github.com/sirupsen/logrus"
-	"go.uber.org/fx"
 )
 
-type StartWatcherInput struct {
-	fx.In
-	Ctx     context.Context
-	Host    host.Host          // 网络主机
-	IsState *config.IsAppState // 应用状态
-	Watcher *fsnotify.Watcher  // 文件监听
-}
-
-// // 定义回调函数类型
-// type FileChangedCallback func(path string)
-
 // 启动观察器
-func StartWatcher(input StartWatcherInput) error {
-	path := filepath.Join(config.SlicePath, input.Host.ID().String())
-	if err := dir.DirExistsAndMkdirAll(path); err != nil {
+func StartWatcher(name string) error {
+	if err := paths.DirExistsAndMkdirAll(name); err != nil {
 		logrus.Errorf("检查路径失败: %v", err)
 		return err
 	}
 
+	// Create new watcher.
+	watcher, err := NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
+
 	// 添加路径
-	if err := input.Watcher.Add(path); err != nil {
+	if err := watcher.Add(name); err != nil {
 		logrus.Errorf("添加路径失败: %v", err)
 		return err
 	}
@@ -45,10 +34,10 @@ func StartWatcher(input StartWatcherInput) error {
 	go func() {
 		for {
 			select {
-			case event, ok := <-input.Watcher.Events:
+			case event, ok := <-watcher.Events:
 				if ok {
 					// Has 报告此事件是否具有给定的操作。
-					if event.Has(fsnotify.Create) {
+					if event.Has(Create) {
 						//	logrus.Println("创建文件:", event.Name)
 						// 调用回调函数处理文件创建事件
 						//callback(event.Name) // 调用回调函数处理逻辑
@@ -58,7 +47,7 @@ func StartWatcher(input StartWatcherInput) error {
 					}
 				}
 
-			case err, ok := <-input.Watcher.Errors:
+			case err, ok := <-watcher.Errors:
 				if !ok {
 					logrus.Errorf("监听文件失败: %v", err)
 					return
@@ -71,18 +60,18 @@ func StartWatcher(input StartWatcherInput) error {
 }
 
 // 新观察者
-func NewWatcher(lc fx.Lifecycle) (*fsnotify.Watcher, error) {
-	// NewWatcher 与底层操作系统建立一个新的观察者并开始等待事件。
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
+// func NewWatcher(lc fx.Lifecycle) (*fsnotify.Watcher, error) {
+// 	// NewWatcher 与底层操作系统建立一个新的观察者并开始等待事件。
+// 	watcher, err := fsnotify.NewWatcher()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	lc.Append(fx.Hook{
-		OnStop: func(ctx context.Context) error {
-			return watcher.Close()
-		},
-	})
+// 	lc.Append(fx.Hook{
+// 		OnStop: func(ctx context.Context) error {
+// 			return watcher.Close()
+// 		},
+// 	})
 
-	return watcher, nil
-}
+// 	return watcher, nil
+// }
